aa442730b1a2f5d7c8a9c6ae76d84ae9aa881b39
Adam Zwakenberg <shamist@gmail.com>
2020-06-13 14:11:48 -0400

    added joypixels font

--- config.def.h
+++ config.def.h
@@ -10,7 +10,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[]          = { "monospace:size=10", "JoyPixels:pixelsize=10:antialias=true:autohint=true"  };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -26,6 +26,8 @@ static const char *colors[][3]      = {
 static const char *const autostart[] = {
 	"picom", NULL,
 	"udiskie", "--tray", NULL,
+	"nitrogen", "--restore", NULL,
+	"dwmblocks", NULL,
 	"st", NULL,
 	NULL /* terminate */
 };
